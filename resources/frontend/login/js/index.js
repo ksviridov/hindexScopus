4b12eb88ba021c8a95de99592b3f429b7058e8b7
lazaryan <lazaryan99@gmail.com>
2020-08-26 01:13:09 +0300

    add error block

--- resources/frontend/login/js/index.js
+++ resources/frontend/login/js/index.js
@@ -1,1 +1,25 @@
-console.log('start')
+;(() => {
+    const fields = document.querySelectorAll('.js-form-item__field')
+    const errorBlock = document.querySelector('.js-error')
+
+    errorBlock.querySelector('.js-error__close').addEventListener('click', () => {
+        errorBlock.style.display = 'none'
+    })
+
+    const error = message => {
+        errorBlock.style.display = 'flex'
+        errorBlock.querySelector('.js-error__message').innerHTML = message
+    }
+
+    document.querySelector('.js-form__submit').addEventListener('click', () => {
+        let data = {}
+
+        fields.forEach(item => data[item.name] = item.value)
+
+        if (!Object.values(data).every(Boolean)) {
+            error('Не все поля заполнены!')
+        } else {
+
+        }
+    })
+})();
