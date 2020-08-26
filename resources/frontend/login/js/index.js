babe82a6887b29038d79d4d597d255cf0556e9fe
lazaryan <lazaryan99@gmail.com>
2020-08-26 09:59:22 +0300

    send request for login

--- resources/frontend/login/js/index.js
+++ resources/frontend/login/js/index.js
@@ -2,9 +2,9 @@
     const fields = document.querySelectorAll('.js-form-item__field')
     const errorBlock = document.querySelector('.js-error')
 
-    errorBlock.querySelector('.js-error__close').addEventListener('click', () => {
-        errorBlock.style.display = 'none'
-    })
+    const closeErrorBlock = () => errorBlock.style.display = 'none'
+
+    errorBlock.querySelector('.js-error__close').addEventListener('click', closeErrorBlock)
 
     const error = message => {
         errorBlock.style.display = 'flex'
@@ -14,12 +14,16 @@
     document.querySelector('.js-form__submit').addEventListener('click', () => {
         let data = {}
 
+        closeErrorBlock()
+
         fields.forEach(item => data[item.name] = item.value)
 
         if (!Object.values(data).every(Boolean)) {
             error('Не все поля заполнены!')
         } else {
-
+            axios.post('/login', data)
+                .then()
+                .catch(error)
         }
     })
 })();
